bf39fb01372aa09ef6667a37684fa80e456409f0
patrick <wyp4wj@gmail.com>
2019-07-06 09:22:58 -0700

    create handleChange in App component, passed onChange to TodoItem component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,33 @@ require('./style.css')
 class App extends React.Component {
   constructor() {
     super()
-    this.state = { todosData }
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+
+      return {
+        todos: updatedTodos
+      }
+    })
   }
 
   render() {
-    const todoItems = this.state.todosData.map(item => <TodoItem key={item.id} item={item} />)
+    const todoItems = this.state.todos.map(item =>
+      <TodoItem
+        key={item.id}
+        item={item}
+        handleChange={this.handleChange} />)
 
     return (
       <div className="todo-list">
